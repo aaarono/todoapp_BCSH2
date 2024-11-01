6169bd64eeb58427235543916807be8294b8d6a3
Oleksandr <120096594+aaarono@users.noreply.github.com>
2024-11-01 20:13:41 +0100

    small fix

--- ToDoApp/Views/LoginWindow.xaml.cs
+++ ToDoApp/Views/LoginWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using ToDoApp.ViewModels;
 
 namespace ToDoApp.Views
 {
@@ -22,6 +23,7 @@ namespace ToDoApp.Views
         public LoginWindow()
         {
             InitializeComponent();
+            DataContext = new LoginViewModel();
         }
     }
 }
